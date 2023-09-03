766b48865328cdc997e3be570fa08207cfdd2105
rteeter1618 <rteeter1618@gmail.com>
2023-09-02 23:07:55 -0400

    Moved defenitions of PayoffCalculators for chicken and games out of models.py into PayoffCalculatorData class

--- prisonersDilemnaWeb/games/gameUtils/payoffCalculator.py
+++ prisonersDilemnaWeb/games/gameUtils/payoffCalculator.py
@@ -21,3 +21,40 @@ class TwoPlayerPayoffCalculator:
         return [myPayoff, theirPayoff]
 
 
+class PayoffCalculatorData:
+    def getPayoffCalculator(self, game_id):
+        #Prisoners Dilemma
+        if(game_id == 1):
+            # "I" choose the rows
+            myPayMatrix = [
+                #C  D
+                [3, 0], #C
+                [5, 1]  #D
+            ]
+            theirPayMatrix = [
+                [3, 5],
+                [0, 1]
+            ]
+            stringToPayoffMap = {}
+            stringToPayoffMap['C'] = 0
+            stringToPayoffMap['D'] = 1
+            return TwoPlayerPayoffCalculator(myPayMatrix, theirPayMatrix, stringToPayoffMap)
+        #Chicken
+        elif(game_id == 2):
+            # "I" choose the rows
+            myPayMatrix = [
+                #S  D
+                [3, 1], #S
+                [5, 0]  #D
+            ]
+            theirPayMatrix = [
+                [3, 5],
+                [1, 0]
+            ]
+            stringToPayoffMap = {}
+            stringToPayoffMap['S'] = 0
+            stringToPayoffMap['D'] = 1
+            return TwoPlayerPayoffCalculator(myPayMatrix, theirPayMatrix, stringToPayoffMap)
+    
+
+
